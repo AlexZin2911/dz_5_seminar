8c73e535646c0446f46de16a490e0e7a65e4f70a
Alex <leschazin4encko@yandex.ru>
2022-09-22 00:45:38 +0400

    added txt code task36

---
+++ task36/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях. [3, 7, 23, 12] -> 19, [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите число, обозначающее количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[count]; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-1000, 1001);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+int OddIndSum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        OddIndSum += array[i];
+    }
+}
+Console.WriteLine($"Сумма элементов, находящихся на нечетных позициях -> {OddIndSum}");
+Console.WriteLine();
